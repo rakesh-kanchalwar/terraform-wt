0ff9620c57a0872d055289a89ed0419dc0927131
Rakesh Kanchalwar <rakeshk@sela.co.in>
2022-06-19 11:46:47 +0530

    Final working deployment - Main Asgn + bonus 1

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,23 @@
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
-}
-
 output "public_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
 
-output "private_ip" {
-  value = azurerm_network_interface.private_nic.private_ip_address
+output "password_0" {
+  value = random_password.password[0].result
+  sensitive = true
+}
+
+output "password_1" {
+  value = random_password.password[1].result
+  sensitive = true
+}
+
+output "password_2" {
+  value = random_password.password[2].result
+  sensitive = true
+}
+
+output "password_db" {
+  value = random_password.password_db.result
+  sensitive = true
 }
